d5bfd2e0685a6d84d64e7c79af6a834f991cc18e
Yasin S <yasinofficialmail@gmail.com>
2025-10-26 20:08:57 +0530

    Implement query for ranking products by revenue
    Add SQL query to identify products with above-average revenue per unit and rank them by category.

--- zepto.sql
+++ zepto.sql
@@ -206,7 +206,43 @@ WHERE stock_percentile <= 0.10
 ORDER BY total_quantity DESC ;
 
 -- Q11. Identify products with above-average revenue per unit and rank them by category.
-    
+-- per unit revenue  
+-- average revenue 
+-- above avg and rank then !
+
+WITH revenue_findings AS (
+    SELECT 
+        sku_id,
+        Category,
+        name,
+        mrp,
+        discountPercent,
+        availableQuantity,
+        discountedSellingPrice,
+        weightInGms,
+        outOfStock,
+        quantity,
+        (discountedSellingPrice * quantity) AS total_revenue_per_unit 
+    FROM zepto
+),
+avg_findings AS (
+    SELECT 
+        AVG(total_revenue_per_unit) AS avg_revenue,
+        category
+    FROM revenue_findings
+    GROUP BY category
+)
+SELECT
+    r.sku_id,
+    r.category,
+    r.name,
+    r.total_revenue_per_unit,
+    RANK() OVER(PARTITION BY r.category ORDER BY r.total_revenue_per_unit DESC) AS category_rank
+FROM revenue_findings r
+JOIN avg_findings a
+    ON r.category = a.category
+WHERE r.total_revenue_per_unit > a.avg_revenue
+ORDER BY r.category ASC, category_rank ASC;
 
 
 
